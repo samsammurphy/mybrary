6d30a5a3b6b0eeb991d16a6a563eb724718dd2ca
Sam Murphy <samsammurphy@gmail.com>
2019-07-27 10:08:25 +0100

    initial author routes

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 // routes
-const indexRouter = require('./routes/index.js')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
-// views
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-
-// static files
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    limit: '10mb',
+    extended: false
+  }))
 
 // MongoDB
 const mongoose = require('mongoose')
@@ -27,8 +30,8 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Database connected'))
 
-
-
+// use routers (aka controllers)
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
